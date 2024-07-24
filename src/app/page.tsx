11a12c78264ae487fa6300fa333e083b51f5dc3c
Aman Deep <aman.dp121@gmail.com>
2024-07-24 13:02:31 +0530

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,107 @@
+/** @format */
+
+import PageTitle from "@/components/PageTitle";
+import Image from "next/image";
+import { DollarSign, Users, CreditCard, Activity } from "lucide-react";
+import Card, { CardContent, CardProps } from "@/components/Card";
+import BarChart from "@/components/BarChart";
+import SalesCard, { SalesProps } from "@/components/SalesCard";
+
+const cardData: CardProps[] = [
+  {
+    label: "Total Revenue",
+    amount: "$45,231.89",
+    discription: "+20.1% from last month",
+    icon: DollarSign
+  },
+  {
+    label: "Subscriptions",
+    amount: "+2350",
+    discription: "+180.1% from last month",
+    icon: Users
+  },
+  {
+    label: "Sales",
+    amount: "+12,234",
+    discription: "+19% from last month",
+    icon: CreditCard
+  },
+  {
+    label: "Active Now",
+    amount: "+573",
+    discription: "+201 since last hour",
+    icon: Activity
+  }
+];
+
+const uesrSalesData: SalesProps[] = [
+  {
+    name: "Olivia Martin",
+    email: "olivia.martin@email.com",
+    saleAmount: "+$1,999.00"
+  },
+  {
+    name: "Jackson Lee",
+    email: "isabella.nguyen@email.com",
+    saleAmount: "+$1,999.00"
+  },
+  {
+    name: "Isabella Nguyen",
+    email: "isabella.nguyen@email.com",
+    saleAmount: "+$39.00"
+  },
+  {
+    name: "William Kim",
+    email: "will@email.com",
+    saleAmount: "+$299.00"
+  },
+  {
+    name: "Sofia Davis",
+    email: "sofia.davis@email.com",
+    saleAmount: "+$39.00"
+  }
+];
+
+export default function Home() {
+  return (
+    <div className="flex flex-col gap-5  w-full">
+      <PageTitle title="Dashboard" />
+      <section className="grid w-full grid-cols-1 gap-4 gap-x-8 transition-all sm:grid-cols-2 xl:grid-cols-4">
+        {cardData.map((d, i) => (
+          <Card
+            key={i}
+            amount={d.amount}
+            discription={d.discription}
+            icon={d.icon}
+            label={d.label}
+          />
+        ))}
+      </section>
+      <section className="grid grid-cols-1  gap-4 transition-all lg:grid-cols-2">
+        <CardContent>
+          <p className="p-4 font-semibold">Overview</p>
+
+          <BarChart />
+        </CardContent>
+        <CardContent className="flex justify-between gap-4">
+          <section>
+            <p>Recent Sales</p>
+            <p className="text-sm text-gray-400">
+              You made 265 sales this month.
+            </p>
+          </section>
+          {uesrSalesData.map((d, i) => (
+            <SalesCard
+              key={i}
+              email={d.email}
+              name={d.name}
+              saleAmount={d.saleAmount}
+            />
+          ))}
+        </CardContent>
+
+        {/*  */}
+      </section>
+    </div>
+  );
+}
